4e90c74ae31dac046c8a1870827432395b7cc7c3
yeswanthannadata <annadatayeswanth@gmail.com>
2021-01-02 18:56:33 +0530

    first GET API dummy

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name: 'Yesh'},
+    {name: 'Yellow'}
+];
+
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'users are returned',
+        users: mockUserData
+    })
+})
+
 app.listen(8000, function(){
     console.log('server is running');
 });
